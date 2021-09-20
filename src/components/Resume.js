29b34d26145a470522556bc929f3ee9ebd25c3c9
MXDragon <bentzbry@gmail.com>
2021-09-19 22:18:29 -0400

    AddResume

---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div className="center">
+      <h3>Proficiencies</h3>
+      <p>Linux, HTML5, CSS3, JavaScript, jQuery Node.js, Express.js, MySQL, NoSQL, Sequelize, Handlebars.js, MongoDB, React, Apollo GraphQL</p>  
+      <h3>Find my resume{" "}
+        <a href="https://docs.google.com/document/d/1GwegyrcV8Ljaij0qi808kFWp_QSpZE9v/edit?usp=sharing&ouid=113017374205306200774&rtpof=true&sd=true">
+          HERE
+        </a>
+      </h3>
+    </div>
+  )
+}
