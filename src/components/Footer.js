bce9f45d268b1b2facd3b2e210d6fad7c7c63dd4
MXDragon <bentzbry@gmail.com>
2021-09-19 22:14:17 -0400

    AddFooter

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="https://github.com/MXDragon"><FaGithub /></a>
+      <a href="https://www.linkedin.com/in/bryan-bentz-a2323641/"><FaLinkedin  /></a>
+    </footer>
+  )
+}
