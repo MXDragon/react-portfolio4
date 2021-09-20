dcf6cdda4bfaaca0bbbe5816b8c92c65890c0c45
MXDragon <bentzbry@gmail.com>
2021-09-19 23:12:44 -0400

    Updated CSS

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ import selfie from '../assets/images/chess_bio.jpg'
 export default function AboutMe() {
   return (
     <>
-      <img src={selfie} className="selfie" alt="Curtis Drullinger" />
+      <img src={selfie} className="selfie" alt="Bryan Bentz" />
       <p>I am a passionate developer, proud dad, linux and javascript guru. I want to work for you.</p>
       <p>I know how to think fast and type fast for you. Let me develop your next websites
         I have been on computers for more than 30 years, with many years of experience in system administration, and my first website design when I was in high school
